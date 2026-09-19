4e0eefad00242b22c60339e03e853de5d444b05a
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-20 03:13:00 +0900

    Add game systems foundation: web/src/game/home.js

---
+++ web/src/game/home.js
@@ -0,0 +1,2 @@
+export const HOME_MODES=['PLAY','GACHA','ROSTER','TRAINING','COLLECTION','SETTINGS'];
+export function createHomeState(){return{screen:'home',selected:'PLAY',currency:1000,collection:[],team:[]}}
