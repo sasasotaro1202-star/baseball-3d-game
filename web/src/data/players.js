79761170e43d93eec01307248ebff4e7b4397cfd
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-20 04:29:40 +0900

    feat: rebalance player ratings and add achievement-based abilities

--- web/src/data/players.js
+++ web/src/data/players.js
@@ -1,21 +1,29 @@
-// Clean-room "console baseball game" rating model.
-// 1-99 scale. Ratings are original game-design translations of real performance,
-// role, tools and era context; they do not reproduce any proprietary game's database.
+// Clean-room console-baseball ratings. 1-99 scale; deliberately compressed.
+// Ratings are original game-design translations of documented career performance.
+// They are not copied from any proprietary baseball game's database.
 export const HISTORIC_PLAYERS=[
-{id:1,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Babe%20Ruth%20%28headshotcropped%29.jpg",name:'Babe Ruth',era:'1900s-1930s',pos:'OF/P',rarity:'LEGEND',skinColor:0xf0b79a,build:'stocky',batting:'left',signature:'ruth',
- power:99,contact:91,field:72,speed:73,arm:88,control:86,stamina:91,vision:94},
-{id:2,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Jackie%20Robinson%20in%201947.jpg",name:'Jackie Robinson',era:'1940s-1950s',pos:'2B',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'robinson',
- power:78,contact:91,field:91,speed:95,arm:88,control:70,stamina:91,vision:94},
-{id:3,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Willie%20Mays%20%281955%29.jpg",name:'Willie Mays',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'mays',
- power:96,contact:94,field:99,speed:96,arm:97,control:70,stamina:96,vision:94},
-{id:4,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Hank%20Aaron%20%281954%29.jpg",name:'Hank Aaron',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'aaron',
- power:97,contact:95,field:89,speed:82,arm:92,control:70,stamina:96,vision:96},
-{id:5,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Shohei%20Ohtani%20%2852251772978%29.jpg",name:'Shohei Ohtani',era:'2020s',pos:'DH/P',rarity:'LEGEND',
- power:99,contact:88,field:55,speed:91,arm:99,control:96,stamina:92,vision:91},
-{id:6,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ichiro%20Suzuki%20%2851007139572%29%20%28cropped%29.jpg",name:'Ichiro Suzuki',era:'2000s-2010s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'left',signature:'ichiro',
- power:73,contact:99,field:97,speed:99,arm:96,control:70,stamina:98,vision:99},
-{id:7,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Sadaharu-Oh-1.jpg",name:'Sadaharu Oh',era:'1960s-1980s',pos:'1B',rarity:'LEGEND',
- power:99,contact:93,field:86,speed:65,arm:82,control:70,stamina:96,vision:97},
-{id:8,image:"https://commons.wikimedia.org/wiki/Special:FilePath/1913%20Ty%20Cobb.png",name:'Ty Cobb',era:'1900s-1920s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'right',signature:'cobb',
- power:76,contact:99,field:94,speed:99,arm:86,control:70,stamina:97,vision:98}
+{id:1,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Babe%20Ruth%20%28headshotcropped%29.jpg",name:'Babe Ruth',era:'1900s-1930s',pos:'OF/P',rarity:'LEGEND',rank:'S',skinColor:0xf0b79a,build:'stocky',batting:'left',signature:'ruth',
+power:94,contact:86,field:66,speed:67,arm:83,control:78,stamina:86,vision:89,
+achievements:['通算714本塁打','12度の本塁打王','通算打点2214'],abilities:[['power_hitter',.08],['clutch',.03],['walk_machine',.04],['pull_power',.03],['big_game',.04],['durability',.03],['legend_power',.05]]},
+{id:2,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Jackie%20Robinson%20in%201947.jpg",name:'Jackie Robinson',era:'1940s-1950s',pos:'2B',rarity:'LEGEND',rank:'A',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'robinson',
+power:72,contact:86,field:86,speed:91,arm:83,control:60,stamina:87,vision:89,
+achievements:['新人王','MVP','6度のオールスター'],abilities:[['contact_hitter',.05],['speedster',.05],['base_running',.05],['first_step',.04],['clutch',.03],['leadership',.04],['big_game',.03]]},
+{id:3,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Willie%20Mays%20%281955%29.jpg",name:'Willie Mays',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',rank:'S',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'mays',
+power:92,contact:89,field:94,speed:91,arm:92,control:60,stamina:91,vision:90,
+achievements:['通算660本塁打','24度のオールスター選出','12度のゴールドグラブ'],abilities:[['power_hitter',.05],['contact_hitter',.04],['range',.06],['strong_arm',.05],['center_field',.06],['big_game',.04],['legend_field',.06],['legend_complete',.04]]},
+{id:4,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Hank%20Aaron%20%281954%29.jpg",name:'Hank Aaron',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',rank:'S',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'aaron',
+power:93,contact:90,field:82,speed:76,arm:86,control:60,stamina:92,vision:92,
+achievements:['通算755本塁打','通算2297打点','15度のオールスター'],abilities:[['power_hitter',.06],['contact_hitter',.05],['consistency',.06],['durability',.05],['big_game',.05],['clutch',.04],['legend_power',.06]]},
+{id:5,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Shohei%20Ohtani%20%2852251772978%29.jpg",name:'Shohei Ohtani',era:'2020s',pos:'DH/P',rarity:'LEGEND',rank:'S',skinColor:0xf0b79a,build:'athletic',batting:'left',signature:'ohtani',
+power:94,contact:81,field:50,speed:84,arm:94,control:88,stamina:88,vision:85,
+achievements:['MVP受賞','投打二刀流','本塁打王'],abilities:[['power_hitter',.06],['velocity',.06],['strikeout',.04],['two_way',.10],['big_game',.04],['stamina',.04],['legend_power',.05],['legend_complete',.03]]},
+{id:6,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ichiro%20Suzuki%20%2851007139572%29%20%28cropped%29.jpg",name:'Ichiro Suzuki',era:'2000s-2010s',pos:'OF',rarity:'LEGEND',rank:'S',skinColor:0xf0b79a,build:'lean',batting:'left',signature:'ichiro',
+power:66,contact:94,field:91,speed:94,arm:91,control:60,stamina:95,vision:95,
+achievements:['MLB通算3089安打','新人王・MVP','10度のゴールドグラブ'],abilities:[['contact_hitter',.07],['leadoff',.06],['speedster',.06],['base_running',.06],['range',.05],['strong_arm',.05],['durability',.06],['legend_contact',.07],['legend_speed',.06]]},
+{id:7,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Sadaharu-Oh-1.jpg",name:'Sadaharu Oh',era:'1960s-1980s',pos:'1B',rarity:'LEGEND',rank:'S',skinColor:0xf0b79a,build:'athletic',batting:'left',signature:'oh',
+power:93,contact:87,field:78,speed:55,arm:75,control:60,stamina:90,vision:91,
+achievements:['NPB通算868本塁打','15度の本塁打王','9度のMVP'],abilities:[['power_hitter',.07],['walk_machine',.05],['clutch',.05],['consistency',.06],['big_game',.05],['durability',.06],['legend_power',.07]]},
+{id:8,image:"https://commons.wikimedia.org/wiki/Special:FilePath/1913%20Ty%20Cobb.png",name:'Ty Cobb',era:'1900s-1920s',pos:'OF',rarity:'LEGEND',rank:'S',skinColor:0xf0b79a,build:'lean',batting:'left',signature:'cobb',
+power:67,contact:95,field:88,speed:95,arm:79,control:60,stamina:93,vision:94,
+achievements:['通算4189安打','打率.366','12度の首位打者'],abilities:[['contact_hitter',.08],['leadoff',.06],['speedster',.07],['base_running',.07],['range',.05],['clutch',.04],['legend_contact',.07],['legend_speed',.07]]}
 ];
