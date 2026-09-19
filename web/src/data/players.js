9947de64c95e91b94de4f973235bc37c29e9c0ff
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-20 03:12:56 +0900

    Add game systems foundation: web/src/data/players.js

---
+++ web/src/data/players.js
@@ -0,0 +1,10 @@
+export const HISTORIC_PLAYERS=[
+{id:1,name:'Babe Ruth',era:'1900s-1930s',pos:'OF/P',power:99,contact:92,control:80},
+{id:2,name:'Jackie Robinson',era:'1940s-1950s',pos:'2B',power:82,contact:94,field:95},
+{id:3,name:'Willie Mays',era:'1950s-1970s',pos:'OF',power:96,contact:94,field:99},
+{id:4,name:'Hank Aaron',era:'1950s-1970s',pos:'OF',power:97,contact:96,field:91},
+{id:5,name:'Shohei Ohtani',era:'2020s',pos:'DH/P',power:99,contact:95,control:96},
+{id:6,name:'Ichiro Suzuki',era:'2000s-2010s',pos:'OF',power:82,contact:99,field:98},
+{id:7,name:'Sadaharu Oh',era:'1960s-1980s',pos:'1B',power:99,contact:95,field:91},
+{id:8,name:'Ty Cobb',era:'1900s-1920s',pos:'OF',power:82,contact:99,field:94}
+];
