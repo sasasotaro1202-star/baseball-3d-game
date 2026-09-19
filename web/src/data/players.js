37f7b7398c0bd779c2f65370f615d9dc136b9f46
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-20 04:19:12 +0900

    Add sourced player portraits to cards

--- web/src/data/players.js
+++ web/src/data/players.js
@@ -2,20 +2,20 @@
 // 1-99 scale. Ratings are original game-design translations of real performance,
 // role, tools and era context; they do not reproduce any proprietary game's database.
 export const HISTORIC_PLAYERS=[
-{id:1,name:'Babe Ruth',era:'1900s-1930s',pos:'OF/P',rarity:'LEGEND',skinColor:0xf0b79a,build:'stocky',batting:'left',signature:'ruth',
+{id:1,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Babe%20Ruth%20%28headshotcropped%29.jpg",name:'Babe Ruth',era:'1900s-1930s',pos:'OF/P',rarity:'LEGEND',skinColor:0xf0b79a,build:'stocky',batting:'left',signature:'ruth',
  power:99,contact:91,field:72,speed:73,arm:88,control:86,stamina:91,vision:94},
-{id:2,name:'Jackie Robinson',era:'1940s-1950s',pos:'2B',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'robinson',
+{id:2,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Jackie%20Robinson%20in%201947.jpg",name:'Jackie Robinson',era:'1940s-1950s',pos:'2B',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'robinson',
  power:78,contact:91,field:91,speed:95,arm:88,control:70,stamina:91,vision:94},
-{id:3,name:'Willie Mays',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'mays',
+{id:3,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Willie%20Mays%20%281955%29.jpg",name:'Willie Mays',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'mays',
  power:96,contact:94,field:99,speed:96,arm:97,control:70,stamina:96,vision:94},
-{id:4,name:'Hank Aaron',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'aaron',
+{id:4,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Hank%20Aaron%20%281954%29.jpg",name:'Hank Aaron',era:'1950s-1970s',pos:'OF',rarity:'LEGEND',skinColor:0x9a5b3e,build:'athletic',batting:'right',signature:'aaron',
  power:97,contact:95,field:89,speed:82,arm:92,control:70,stamina:96,vision:96},
-{id:5,name:'Shohei Ohtani',era:'2020s',pos:'DH/P',rarity:'LEGEND',
+{id:5,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Shohei%20Ohtani%20%2852251772978%29.jpg",name:'Shohei Ohtani',era:'2020s',pos:'DH/P',rarity:'LEGEND',
  power:99,contact:88,field:55,speed:91,arm:99,control:96,stamina:92,vision:91},
-{id:6,name:'Ichiro Suzuki',era:'2000s-2010s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'left',signature:'ichiro',
+{id:6,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ichiro%20Suzuki%20%2851007139572%29%20%28cropped%29.jpg",name:'Ichiro Suzuki',era:'2000s-2010s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'left',signature:'ichiro',
  power:73,contact:99,field:97,speed:99,arm:96,control:70,stamina:98,vision:99},
-{id:7,name:'Sadaharu Oh',era:'1960s-1980s',pos:'1B',rarity:'LEGEND',
+{id:7,image:"https://commons.wikimedia.org/wiki/Special:FilePath/Sadaharu-Oh-1.jpg",name:'Sadaharu Oh',era:'1960s-1980s',pos:'1B',rarity:'LEGEND',
  power:99,contact:93,field:86,speed:65,arm:82,control:70,stamina:96,vision:97},
-{id:8,name:'Ty Cobb',era:'1900s-1920s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'right',signature:'cobb',
+{id:8,image:"https://commons.wikimedia.org/wiki/Special:FilePath/1913%20Ty%20Cobb.png",name:'Ty Cobb',era:'1900s-1920s',pos:'OF',rarity:'LEGEND',skinColor:0xf0b79a,build:'lean',batting:'right',signature:'cobb',
  power:76,contact:99,field:94,speed:99,arm:86,control:70,stamina:97,vision:98}
 ];
